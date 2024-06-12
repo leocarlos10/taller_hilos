9c211c5ac7b7787a8f989565b615029105468f04
leocarlos10 <leocarlosospinacausil10@gmail.com>
2024-06-11 23:40:18 -0500

    1 contexto listo

--- taller_hilos/src/Modelo/Hilo_vehiculo.java
+++ taller_hilos/src/Modelo/Hilo_vehiculo.java
@@ -4,6 +4,7 @@
  */
 package Modelo;
 
+import javafx.application.Platform;
 import javafx.scene.image.ImageView;
 
 /**
@@ -11,27 +12,69 @@ import javafx.scene.image.ImageView;
  * @author USUARIO
  */
 public class Hilo_vehiculo extends Thread {
-    
+
     String nombreCarro;
     double posicionInicial;
     int velocidad;
-    ImageView imagen;
-    Long tiempoInicial;
-    
-    public Hilo_vehiculo(String nombre, double posicionInicial,int velocidad,ImageView imagen, Long tiempoInicial){
+    ImageView imagen, carro1, carro2;
+    long tiempoInicial;
+    boolean running;
+
+    public Hilo_vehiculo(String nombre, double posicionInicial, int velocidad, ImageView imagen, long tiempoInicial, ImageView carro1, ImageView carro2) {
         this.nombreCarro = nombre;
         this.velocidad = velocidad;
         this.posicionInicial = posicionInicial;
         this.imagen = imagen;
-        this.tiempoInicial= tiempoInicial;
+        this.tiempoInicial = tiempoInicial;
+        this.running = true;
+        this.carro1 = carro1;
+        this.carro2 = carro2;
     }
-    
-    
+
     @Override
-    public void run(){
-        
-        //logica
+    public void run() {
+        movimiento();
+    }
+
+    public void movimiento() {
+
+        while (running) {
+            try {
+                Thread.sleep(200); // Control de la frecuencia de actualización
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // Calcular el tiempo transcurrido
+            long tiempoTranscurrido = System.currentTimeMillis();
+            double tiempoSegundos = (tiempoTranscurrido - tiempoInicial) / 1000.0; // Tiempo en segundos
+
+            // Calcular el nuevo desplazamiento usando x = vt
+            double desplazamiento = velocidad * tiempoSegundos;
+
+            // si el hilo que ejecuta el metodo es carro1 se suma de lo contrario se resta
+            double newX;
+            if (nombreCarro.equals("carro1")) {
+                newX = posicionInicial + desplazamiento;
+            } else {
+                newX = posicionInicial - desplazamiento;
+            }
+
+            // Actualizar la posición del carro
+            Platform.runLater(() -> {
+                imagen.setLayoutX(newX); // Actualizar la posición en X
+
+                // Comprobar colisión
+                if (carro1.getBoundsInParent().intersects(carro2.getBoundsInParent())) {
+                    detener();
+                }
+            });
+        }
     }
     
-    
+    public synchronized void detener(){
+        
+       this.running = false; 
+    }
+
 }
--- taller_hilos/src/controlador/FXMLDocumentController.java
+++ taller_hilos/src/controlador/FXMLDocumentController.java
@@ -10,7 +10,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
@@ -22,7 +22,11 @@ public class FXMLDocumentController implements Initializable {
     
     double posicionInicialCarro1;
     double posicionInicialCarro2;
-    Long tiempoIncial;
+    long tiempoIncial;
+    
+    //hilos
+     Hilo_vehiculo hilo1;
+     Hilo_vehiculo hilo2;
     
        @FXML
     private ImageView carro1;
@@ -40,8 +44,8 @@ public class FXMLDocumentController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        posicionInicialCarro1 = carro1.getX();
-        posicionInicialCarro2 = carro2.getX();
+        posicionInicialCarro1 = carro1.getLayoutX();
+        posicionInicialCarro2 = carro2.getLayoutX();
         
         tiempoIncial = System.currentTimeMillis();
         
@@ -49,9 +53,9 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     void event_guardarV(ActionEvent event) {
-        
-        Hilo_vehiculo hilo1 = new Hilo_vehiculo("carro1",carro1.getX(),Integer.parseInt(VCarro1.getText()), carro1,tiempoIncial);
-        Hilo_vehiculo hilo2 = new Hilo_vehiculo("carro2",carro2.getX(),Integer.parseInt(VCarro2.getText()), carro2,tiempoIncial);
+        detenerHilos();
+        hilo1 = new Hilo_vehiculo("carro1", carro1.getLayoutX(), Integer.parseInt(VCarro1.getText()), carro1, tiempoIncial, carro1, carro2);
+        hilo2 = new Hilo_vehiculo("carro2", carro2.getLayoutX(), Integer.parseInt(VCarro2.getText()), carro2, tiempoIncial, carro1, carro2);
         hilo1.start();
         hilo2.start();
     }
@@ -68,7 +72,21 @@ public class FXMLDocumentController implements Initializable {
     
      @FXML
     void event_reiniciar(ActionEvent event) {
-
+            detenerHilos();
+            carro1.setLayoutX(posicionInicialCarro1);
+            carro2.setLayoutX(posicionInicialCarro2);
+            tiempoIncial=System.currentTimeMillis();
     }
     
+     private void detenerHilos() {
+        if (hilo1 != null) {
+            hilo1.detener();
+            hilo1 = null;
+        }
+        
+        if (hilo2 != null) {
+            hilo2.detener();
+            hilo2 = null;
+        }
+    }
 }
